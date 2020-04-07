257e5b4e04e8d082935ca0af5a59fa3d69e69983
karp7 <akarp762@gmail.com>
2020-04-08 01:23:25 +0300

    v 7

--- storage/models.py
+++ storage/models.py
@@ -23,7 +23,7 @@ class Project(models.Model):
 
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to=settings.MEDIA_ROOT)
+    document = models.FileField(upload_to='documents/')
     # uploaded_at = models.DateTimeField(auto_now_add=True)
     prjog = models.ForeignKey(Project,on_delete=models.CASCADE)
 
